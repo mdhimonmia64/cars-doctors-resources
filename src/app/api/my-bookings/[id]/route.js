0286b8b24119e359869878427ff1fd160a014608
Himon <mdhimonmia64@gmail.com>
2026-06-09 20:08:32 +0600

    added middleware(proxy)

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,29 +1,51 @@
+import { authOptions } from "@/lib/authOption";
 import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server"
 
-export const GET = async (req, {params}) => {
+export const GET = async (req, { params }) => {
     const p = await params;
     const bookingCollection = dbConnect(collectionNameObj.bookingCollection);
-    const query = { _id: new ObjectId(p.id)};
+    const query = { _id: new ObjectId(p.id) };
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
     const singleBooking = await bookingCollection.findOne(query);
+    const isOwnerOk = email === singleBooking?.email;
+    if (isOwnerOk) {
+        return NextResponse.json(singleBooking)
+    }
+    else {
+        return NextResponse.json({ message: "Forbidden GET action" }, { status: 403 })
+    }
 
-    return NextResponse.json(singleBooking)
 }
 
-export const PATCH = async (req, {params}) => {
+export const PATCH = async (req, { params }) => {
     const p = await params;
     const bookingCollection = dbConnect(collectionNameObj.bookingCollection);
-    const query = { _id: new ObjectId(p.id)};
-    const body = await req.json();
-    const filter = {
-        $set: {
-            ...body
+    const query = { _id: new ObjectId(p.id) };
+
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    const currentBookingData = await bookingCollection.findOne(query);
+    const isOwnerOk = email === currentBookingData?.email;
+    if (isOwnerOk) {
+        const body = await req.json();
+        const filter = {
+            $set: {
+                ...body
+            }
         }
+        const option = {
+            upsert: true
+        }
+        const updatedBooking = await bookingCollection.updateOne(query, filter, option);
+        return NextResponse.json(updatedBooking);
     }
-    const option = {
-        upsert: true
+    else {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 403 })
     }
-    const updatedBooking = await bookingCollection.updateOne(query, filter, option);
-    return NextResponse.json(updatedBooking);
+
+
 }
--- src/app/my-bookings/[id]/page.jsx
+++ src/app/my-bookings/[id]/page.jsx
@@ -1,9 +1,12 @@
 import BookingUpdateForm from '@/components/forms/BookingUpdateForm'
+import { headers } from 'next/headers';
 import React from 'react'
 
 export default async function UpdateBookingPage({params}) {
     const p = await params;
-    const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`);
+    const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`,{
+      headers: await headers(),
+    });
     const data = await res.json();
   return (
     <div>
